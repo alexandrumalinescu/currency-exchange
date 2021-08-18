8c1d77ca0eeba7347aaa9c086826dd6dded8a1ee
alexandrumalinescu <67902199+alexandrumalinescu@users.noreply.github.com>
2021-08-18 18:23:23 +0300

    fff

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
 import DateComponent from '../src/components/dateComponent/date'
-import CurrencyComponent from './components/currency/CurrencyComponent';
+//import CurrencyComponent from './components/currency/CurrencyComponent';
+import Currency from './components/currency/Currency';
 
-function App() {
+
+const App= ()=> {
 
   return (
     <div className="container">
       <h1 className='title'> Currency Exchange</h1>
       <DateComponent/>
-      <CurrencyComponent/>
+      <Currency className='currency'/>
     </div>
   );
 }
